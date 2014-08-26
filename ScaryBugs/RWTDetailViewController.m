f2932bcac7e896fbbc4760a341cd87d59ab799c5
Cyan <chansonyan@live.com>
2014-08-26 21:52:53 +0800

    learn how to create a detail view so that you can view a larger picture of the bugs, rate them, and change their pictures!
    http://www.raywenderlich.com/1845/ios-tutorial-how-to-create-a-simple-ip
    hone-app-tutorial-part-2

---
+++ ScaryBugs/RWTDetailViewController.m
@@ -0,0 +1,112 @@
+//
+//  RWTDetailViewController.m
+//  ScaryBugs
+//
+//  Created by Jorge Jordán Arenas on 04/02/14.
+//
+//
+
+#import "RWTDetailViewController.h"
+#import "RWTScaryBugDoc.h"
+#import "RWTScaryBugData.h"
+#import "RWTUIImageExtras.h"
+
+@interface RWTDetailViewController ()
+- (void)configureView;
+@end
+
+@implementation RWTDetailViewController
+
+@synthesize picker = _picker;
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+    self.rateView.notSelectedImage = [UIImage imageNamed:@"shockedface2_empty.png"];
+    self.rateView.halfSelectedImage = [UIImage imageNamed:@"shockedface2_half.png"];
+    self.rateView.fullSelectedImage = [UIImage imageNamed:@"shockedface2_full.png"];
+    self.rateView.editable = YES;
+    self.rateView.maxRating = 5;
+    self.rateView.delegate = self;
+    
+    if (self.detailItem) {
+        self.titleField.text = self.detailItem.data.title;
+        self.rateView.rating = self.detailItem.data.rating;
+        self.imageView.image = self.detailItem.fullImage;
+    }
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation {
+    return YES;
+}
+
+- (IBAction)titleFieldTextChanged:(id)sender {
+    self.detailItem.data.title = self.titleField.text;
+}
+
+#pragma mark UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark RWTRateViewDelegate
+
+- (void)rateView:(RWTRateView *)rateView ratingDidChange:(float)rating {
+    self.detailItem.data.rating = rating;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)addPictureTapped:(id)sender {
+    if (self.picker == nil) {
+        self.picker = [[UIImagePickerController alloc] init];
+        self.picker.delegate = self;
+        self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        self.picker.allowsEditing = NO;
+    }
+    [self presentViewController:_picker animated:YES completion:nil];
+}
+
+#pragma mark UIImagePickerControllerDelegate
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    UIImage *fullImage = (UIImage *) [info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImage *thumbImage = [fullImage imageByScalingAndCroppingForSize:CGSizeMake(44, 44)];
+    self.detailItem.fullImage = fullImage;
+    self.detailItem.thumbImage = thumbImage;
+    self.imageView.image = fullImage;
+}
+
+@end
